02e39fb283e9e55d61b594e66b17d2c242b9679a
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-07-01 20:52:19 +0530

    Added

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,11 +2,14 @@ import express from 'express';
 
 import { login } from '../controllers/auth.js';
 import { getAllChanels, updateChanelData } from '../controllers/chanel.js';
+import { updatePoints } from '../controllers/user.js';
 
 const routes = express.Router();
 
 routes.post('/login',login)
 routes.patch('/update/:id',updateChanelData)
 routes.get('/getAllChanels',getAllChanels)
+routes.post('/updatePoints', updatePoints);
+routes.get('/getPoints/:userId', getPoints);
 
 export default routes;
